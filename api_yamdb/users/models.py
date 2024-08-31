1fa331cf766247adfe103571e736297598e47808
Nikita <48051988+qqudra@users.noreply.github.com>
2024-08-31 18:06:50 +0300

    Update models.py
    убрана роль анонима

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 ROLE_VARIANTS = [
-    ('anon', 'anonimous'),
     ('admin', 'admin'),
     ('moderator', 'moderator'),
     ('user', 'user')
